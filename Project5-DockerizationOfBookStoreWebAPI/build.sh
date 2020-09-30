19ff28bcbb04451e117d7d98d9b277629a06f5bc
Adem <newowner@newowners-MacBook-Pro.local>
2020-09-30 12:16:07 -0500

    yml tample updated

--- Project5-DockerizationOfBookStoreWebAPI/build.sh
+++ Project5-DockerizationOfBookStoreWebAPI/build.sh
@@ -4,12 +4,12 @@ yum update -y
 amazon-linux-extras install docker -y
 systemctl start docker
 systemctl enable docker
-user -a -G docker ec2-user
+usermod -a -G docker ec2-user
 newgrp docker
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-FOLDER="https://raw.githubusercontent.com/AdamTekin/clarusway-aws-workshop/master/Project5-Dockerization%20of%20BookStore%20Web%20API/"
+FOLDER="https://raw.githubusercontent.com/AdamTekin/clarusway-aws-workshop/master/Project5-DockerizationOfBookStoreWebAPI/"
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/bookstore-api.py" -L "$FOLDER"bookstore-api.py
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/requirements.txt" -L "$FOLDER"requirements.txt
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/Dockerfile" -L "$FOLDER"Dockerfile
